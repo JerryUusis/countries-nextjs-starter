02d16440c4d3e0db23bc92e0367049bb44132676
JerryU <jerry.uusitalo@gmail.com>
2024-02-29 21:20:13 +0200

    Conditionally render register, login and logout buttons depending is the user signed in or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import { logout } from "../auth/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../auth/firebase";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  if (user) {
+    console.log("Logged in with user:", user);
+  }
   return (
-    <Container fluid >
+    <Container fluid>
       <Row>
         <Navbar bg="light" variant="light">
           <Container className="justify-content-end">
@@ -24,13 +31,18 @@ const Header = () => {
                 <Link to="/favourites">
                   <Button variant="contained">Favourites</Button>
                 </Link>
-                <Link to="/register">
-                  <Button variant="contained" >Register</Button>
-                </Link>
-                <Link to="/login">
-                  <Button variant="contained" >Login</Button>
-                </Link>
-                <Button onClick={logout}>Sign out</Button>
+                {!user ? (
+                  <>
+                    <Link to="/register">
+                      <Button variant="contained">Register</Button>
+                    </Link>
+                    <Link to="/login">
+                      <Button variant="contained">Login</Button>
+                    </Link>
+                  </>
+                ) : (
+                  <Button onClick={logout}>Sign out</Button>
+                )}
               </Nav>
             </Navbar.Collapse>
           </Container>
