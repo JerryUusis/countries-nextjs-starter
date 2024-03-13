37f2022f4bf10f132e15b153c292c1ed85828d07
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-13 09:21:09 +0200

    Initiate theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF5733'
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+  < React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode >
 )
