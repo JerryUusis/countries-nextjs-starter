0ed6248609fb7ea3a949763ce0778d3ff9326f61
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-13 11:08:49 +0200

    Remove themeProvider and theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createTheme, ThemeProvider, CssBaseline } from '@mui/material'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FF5733'
-    }
-  }
-})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
   < React.StrictMode >
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
-    </ThemeProvider>
   </React.StrictMode >
 )
