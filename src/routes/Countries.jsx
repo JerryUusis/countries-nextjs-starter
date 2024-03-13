3dc79f2239590199c2141d14a7daf75be47869bf
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-13 15:30:53 +0200

    Create lanuage formatting function

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -20,14 +20,42 @@ const Countries = () => {
     return (
       <Box
       >
-        <CircularProgress sx={{position:"absolute", top:"50%", left:"50%"}}/>
+        <CircularProgress sx={{ position: "absolute", top: "50%", left: "50%" }} />
       </Box>
     );
   }
 
-
   // Flag is in 2∶1 in aspect ratio. Use this value to adjust the scale the flag size correctly.
   const flagHeight = 130;
+
+  const formatLanguages = (languagesObject) => {
+    if (!languagesObject) {
+      return []
+    }
+    
+    const formattedLanguages = [];
+
+    if (formatLanguages.length === 0) {
+      return [];
+    }
+    const languages = Object.values(languagesObject)
+
+    if (languages.length === 1) {
+      return languages;
+    }
+    for (let i = 0; i < languages.length; i++) {
+      if (languages.length - 1 === i) {
+        formattedLanguages.push(languages[i]);
+      }
+      else if (languages.length - 2 === i) {
+        formattedLanguages.push(languages[i] + " & ")
+      }
+      else {
+        formattedLanguages.push(languages[i] + ", ")
+      }
+    }
+    return formattedLanguages
+  }
 
   return (
     <Box>
@@ -61,7 +89,7 @@ const Countries = () => {
                   </Typography>
                   <List>
                     <ListItem>
-                      {Object.values(country.languages ?? {}).join(", ")}
+                      {formatLanguages(country.languages)}
                     </ListItem>
                     <ListItem>
                       {Object.values(country.currencies || {})
