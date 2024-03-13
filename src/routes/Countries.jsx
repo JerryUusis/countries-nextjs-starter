5d561a82b17b328294733b889acc4a153671f016
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-13 14:30:21 +0200

    Refactor using MUI and add initial styles

--- src/routes/Countries.jsx
+++ src/routes/Countries.jsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
-
+import { Box, TextField, Card, CardMedia, Typography, List, ListItem, CircularProgress } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { Form, Spinner } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
-import Row from "react-bootstrap/Row";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { initializeCountries } from "../store/countriesSlice";
@@ -14,7 +8,6 @@ import { addFavourite } from "../store/favouritesSlice";
 
 const Countries = () => {
   const dispatch = useDispatch();
-
   const countriesList = useSelector((state) => state.countries.countries);
   const loading = useSelector((state) => state.countries.isLoading);
   const [search, setSearch] = useState("");
@@ -23,45 +16,32 @@ const Countries = () => {
     dispatch(initializeCountries());
   }, [dispatch]);
 
-  useEffect(() => {
-    console.log("search: ", search);
-  }, [search]);
-
   if (loading) {
     return (
-      <Col className="text-center m-5">
-        <Spinner
-          animation="border"
-          role="status"
-          className="center"
-          variant="info"
-        >
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Col>
+      <Box
+      >
+        <CircularProgress sx={{position:"absolute", top:"50%", left:"50%"}}/>
+      </Box>
     );
   }
 
+
+  // Flag is in 2∶1 in aspect ratio. Use this value to adjust the scale the flag size correctly.
+  const flagHeight = 130;
+
   return (
-    <Container fluid>
-      <Row>
-        <Form.Control
-          style={{ width: "18rem" }}
-          type="search"
-          className="me-2 "
-          placeholder="Search for countries"
-          aria-label="Search"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </Row>
-      <Row xs={2} md={3} lg={4} className=" g-3">
+    <Box>
+      <Box m={2}>
+        <TextField label="Countries" placeholder="Search for countries" onChange={(e) => setSearch(e.target.value)}></TextField>
+      </Box>
+      <Box sx={{ display: "flex", flexFlow: "wrap", gap: "1rem", justifyContent: "center" }}>
         {countriesList
           .filter((country) =>
             country.name.common.toLowerCase().includes(search.toLowerCase())
           )
           .map((country) => (
-            <Col className="mt-5" key={country.name.common}>
-              <Card className="h-100">
+            <Box key={country.name.common}>
+              <Card sx={{ height: "350px", width: `${flagHeight * 2}px` }}>
                 <FavoriteIcon
                   onClick={() => dispatch(addFavourite(country))}
                 ></FavoriteIcon>
@@ -69,47 +49,35 @@ const Countries = () => {
                   to={`/countries/${country.name.common}`}
                   state={{ country: country }}
                 >
-                  <Card.Img
-                    variant="top"
-                    src={country.flags.svg}
-                    className="rounded h-50"
-                    style={{
-                      objectFit: "cover",
-                      minHeight: "200px",
-                      maxHeight: "200px",
-                    }}
+                  <CardMedia
+                    image={country.flags.svg}
+                    sx={{ height: `${flagHeight}px` }}
                   />
                 </Link>
-                <Card.Body className="d-flex flex-column">
-                  <Card.Title>{country.name.common}</Card.Title>
-                  <Card.Subtitle className="mb-5 text-muted">
+                <Box sx={{ p: "1rem" }}>
+                  <Typography sx={{ fontWeight: "bold" }}>{country.name.common}</Typography>
+                  <Typography sx={{ fontStyle: "italic" }}>
                     {country.name.official}
-                  </Card.Subtitle>
-                  <ListGroup
-                    variant="flush"
-                    className="flex-grow-1 justify-content-end"
-                  >
-                    <ListGroup.Item>
-                      <i className="bi bi-translate me-2"></i>
+                  </Typography>
+                  <List>
+                    <ListItem>
                       {Object.values(country.languages ?? {}).join(", ")}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <i className="bi bi-cash-coin me-2"></i>
+                    </ListItem>
+                    <ListItem>
                       {Object.values(country.currencies || {})
                         .map((currency) => currency.name)
                         .join(", ")}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <i className="bi bi-people me-2"></i>
+                    </ListItem>
+                    <ListItem>
                       {country.population.toLocaleString()}
-                    </ListGroup.Item>
-                  </ListGroup>
-                </Card.Body>
+                    </ListItem>
+                  </List>
+                </Box>
               </Card>
-            </Col>
+            </Box>
           ))}
-      </Row>
-    </Container>
+      </Box>
+    </Box>
   );
 };
 
