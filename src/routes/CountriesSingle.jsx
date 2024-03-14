b732c441fc6aebdacd1a455a060e43ecef391a37
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-14 22:15:04 +0200

    Initialize styles

--- src/routes/CountriesSingle.jsx
+++ src/routes/CountriesSingle.jsx
@@ -1,8 +1,12 @@
-import { Box, Button, Typography, CircularProgress } from "@mui/material";
+import { Box, Button, Typography, CircularProgress, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 const { VITE_OPENWEATHER_API } = import.meta.env;
+import AirIcon from '@mui/icons-material/Air';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 const CountriesSingle = () => {
   const [weather, setWeather] = useState("");
@@ -34,40 +38,82 @@ const CountriesSingle = () => {
     )
   }
 
+  // const formatTime = (timeUnix) => {
+  //   const dateObject = new Date(timeUnix * 1000);
+  //   const hours = dateObject.getUTCHours();
+  //   const minutes = dateObject.getUTCMinutes();
+  //   return `${hours.toString().padStart(2, '0')}.${minutes.toString().padStart(2, '0')}`;
+  // };
+
+
+  // const sunriseTimeUnix = weather.sys.sunrise;
+  // const sunsetTimeUnix = weather.sys.sunset;
+
+  // console.log(weather)
+
+
   return (
-    <Box>
-      <Box >
-        <Box>
-          <Box sx={{
-            backgroundImage: `url(https://source.unsplash.com/featured/1600x900?${country.name.common})`,
-            height: "337.5px",
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            maxWidth: "600px"
-          }}
-            alt={`Picture from ${country.name.common}`}></Box >
-        </Box>
-        <Box>
-          <Typography sx={{ fontWeight: "bold" }}>{country.name.common}</Typography>
-          <Typography>Capital: {country.capital}</Typography>
-          {!error && weather && (
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      gap:"3rem"
+    }}>
+      {/* <Box sx={{
+        backgroundImage: `url(https://source.unsplash.com/featured/1600x900?${country.name.common})`,
+        height: "337.5px",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        maxWidth: "600px"
+      }}
+        alt={`Picture from ${country.name.common}`}>
+      </Box > */}
+      {!error && weather && (
+        <Box >
+          <Box sx={{ display: "flex", gap: "2rem", alignItems: "flex-end" }}>
             <Box>
-              <Typography>
-                Right now it is <strong>{weather.main.temp}</strong>{" "}
-                degrees in {country.capital} and {" "} {weather.weather[0].description}
-              </Typography>
               <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+              <Typography >{weather.weather[0].description}</Typography>
+              <Typography>Humidity {weather.main.humidity}%</Typography>
             </Box>
-          )}
+            <Box>
+              <Typography sx={{ fontWeight: "bold" }}>{country.name.common}</Typography>
+              <Typography>Capital: {country.capital}</Typography>
+            </Box>
+
+          </Box>
         </Box>
-      </Box>
+      )}
       <Box>
-        <Box>
-          <Button variant="contained" color="secondary" onClick={() => navigate("/countries")}>
-            Back to countries
-          </Button>
-        </Box>
+      <Typography>Current Weather</Typography>
+        <TableContainer component={Paper} sx={{ maxWidth: "550px" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell><ThermostatIcon />Temperature</TableCell>
+                <TableCell><AirIcon />Wind</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Current {weather.main.temp}°C</TableCell>
+                <TableCell>{weather.wind.speed} m/s</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Highest {weather.main.temp_max}°C</TableCell>
+                <TableCell>Highest {weather.wind.gust} m/s</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Lowest {weather.main.temp_min}°C</TableCell>
+                <TableCell>Feels like {weather.main.feels_like}°C</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
+      <Button variant="contained" color="secondary" onClick={() => navigate("/countries")}>
+          Back to countries
+        </Button>
     </Box>
   );
 };
