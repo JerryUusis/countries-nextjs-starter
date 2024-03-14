51cf4ef4c34c4c2d32d9017e6760dafd239b63f8
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-14 12:44:32 +0200

    Refactor Favourites to use MUI and CountryCards

--- src/routes/Favourites.jsx
+++ src/routes/Favourites.jsx
@@ -1,19 +1,29 @@
 import { useEffect } from "react";
-
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import { Button } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
-import Row from "react-bootstrap/Row";
+import { Box, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeCountries } from "../store/countriesSlice";
 import { removeFavourite, clearFavourites } from "../store/favouritesSlice";
+import { getFavouritesFromSource } from "../auth/firebase";
+import CountryCard from "../components/CountryCard"
 
 const Favourites = () => {
     const dispatch = useDispatch();
 
     const favourites = useSelector((state) => state.favourites.favourites);
+    let countriesList = useSelector((state) => state.countries.countries);
+
+    if (favourites.length > 0) {
+        countriesList = countriesList.filter((country) =>
+            favourites.includes(country.name.common)
+        );
+    } else {
+        countriesList = [];
+    }
+
+    useEffect(() => {
+        dispatch(initializeCountries());
+        dispatch(getFavouritesFromSource());
+    }, [dispatch]);
 
     const handleRemoveFavourite = (countryName) => {
         dispatch(removeFavourite(countryName))
@@ -23,58 +33,20 @@ const Favourites = () => {
         dispatch(clearFavourites())
     }
 
-    // TODO: Implement logic to retrieve favourites later.
     useEffect(() => {
         dispatch(initializeCountries());
+        dispatch(getFavouritesFromSource());
     }, [dispatch]);
 
     return (
-        <Container fluid>
-            <Button onClick={() => handleClearFavourites()}>Clear favourites</Button>
-            <Row xs={2} md={3} lg={4} className=" g-3">
-                {favourites.map((country) => (
-                    <Col key={country.name.official} className="mt-5">
-                        <Card className="h-100">
-                            <Card.Img
-                                variant="top"
-                                className="rounded h-50"
-                                src={country.flags.svg}
-                                style={{
-                                    objectFit: "cover",
-                                    minHeight: "200px",
-                                    maxHeight: "200px",
-                                }}
-                            />
-                            <Card.Body className="d-flex flex-column">
-                                <Card.Title>{country.name.common}</Card.Title>
-                                <Card.Subtitle className="mb-5 text-muted">
-                                    {country.name.official}
-                                </Card.Subtitle>
-                                <ListGroup
-                                    variant="flush"
-                                    className="flex-grow-1 justify-content-end"
-                                >
-                                    <ListGroup.Item>
-                                        <i className="bi bi-translate me-2"></i>
-                                        {Object.values(country.languages ?? {}).join(", ")}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        <i className="bi bi-cash-coin me-2"></i>
-                                        {Object.values(country.currencies || {})
-                                            .map((currency) => currency.name)
-                                            .join(", ")}
-                                    </ListGroup.Item>
-                                    <ListGroup.Item>
-                                        {country.population.toLocaleString()}
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </Card.Body>
-                            <Button onClick={() => handleRemoveFavourite(country.name.common)}>Remove</Button>
-                        </Card>
-                    </Col>
+        <Box >
+            <Button variant="contained" color="secondary" onClick={() => handleClearFavourites()}>Clear favourites</Button>
+            <Box>
+                {countriesList.map((country) => (
+                    <CountryCard country={country}/>
                 ))}
-            </Row>
-        </Container>
+            </Box>
+        </Box>
     );
 };
 
