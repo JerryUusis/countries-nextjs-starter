ad2145e63fd70ac8848e810aabdf39c84b853577
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-16 12:47:47 +0200

    Update styles

--- src/routes/CountriesSingle.jsx
+++ src/routes/CountriesSingle.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography, CircularProgress, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper } from "@mui/material";
+import { Box, Button, Typography, CircularProgress, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper, useMediaQuery } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -6,8 +6,9 @@ const { VITE_OPENWEATHER_API, VITE_GOOGLE_API, VITE_GOOGLE_MAP_ID } = import.met
 import AirIcon from '@mui/icons-material/Air';
 import ThermostatIcon from '@mui/icons-material/Thermostat';
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow, } from "@vis.gl/react-google-maps"; //https://visgl.github.io/react-google-maps/
-import BedtimeIcon from '@mui/icons-material/Bedtime';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import { formatLanguages, formatCurrencies } from "../components/CountryCard";
+import { initializeCountries } from "../store/countriesSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const CountriesSingle = () => {
   const [weather, setWeather] = useState("");
@@ -17,6 +18,33 @@ const CountriesSingle = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const country = location.state.country;
+  const dispatch = useDispatch();
+  const countriesList = useSelector((state) => state.countries.countries);
+
+  const screenSizeSm = useMediaQuery("(min-width:600px)");
+
+  useEffect(() => {
+    dispatch(initializeCountries())
+  }, [])
+
+  const getNeighbouringCountries = () => {
+    const neighbouringCountries = [];
+
+    if (country.borders.length === 0) {
+      return []
+    }
+
+    for (const countryCioc of country.borders) {
+      countriesList.forEach((country) => {
+        if (countryCioc === country.cioc) {
+          neighbouringCountries.push(`${country.name.common} ${country.flag}`)
+        }
+      })
+    }
+    return neighbouringCountries
+  }
+
+  const neighbouringCountries = formatLanguages(getNeighbouringCountries())
 
   useEffect(() => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${VITE_OPENWEATHER_API}`)
@@ -50,7 +78,8 @@ const CountriesSingle = () => {
   // const sunriseTimeUnix = weather.sys.sunrise;
   // const sunsetTimeUnix = weather.sys.sunset;
   const position = { lat: weather.coord.lat, lng: weather.coord.lon };
-  console.log(country)
+
+  console.log(weather)
 
   return (
     <Box sx={{
@@ -60,43 +89,84 @@ const CountriesSingle = () => {
       gap: "3rem",
       my: "2rem"
     }}>
-      {/* <Box sx={{
-        backgroundImage: `url(https://source.unsplash.com/featured/1600x900?${country.name.common})`,
-        height: "337.5px",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        maxWidth: "600px"
-      }}
-        alt={`Picture from ${country.name.common}`}>
-      </Box > */}
       {!error && weather && (
         <Box >
-          <Box sx={{ display: "flex", gap: "2rem", alignItems: "flex-end" }}>
-            <Box>
-              <Typography sx={{ fontWeight: "bold" }}>{country.name.common} {country.flag}</Typography>
-              <Typography>Capital: {country.capital}</Typography>
+          <Box sx={{}}>
+            <Box sx={{
+              display: "flex", flexDirection: {
+                xs: "column",
+                sm: "row"
+              }, gap: "2rem",
+            }}>
+            </Box>
+            <Box my={"2rem"}>
+              <TableContainer component={Paper} sx={{ maxWidth: "550px" }}>
+                <Table>
+                  <TableHead>
+                    <TableRow sx={{ backgroundColor: "lightGrey" }}>
+                      {screenSizeSm ?
+                        (<>
+                          <TableCell><img src={country.coatOfArms.svg} alt={`${country.name.common} coat of arms`} style={{ height: "200px", padding: "0.25rem" }} /></TableCell>
+                          <TableCell><img src={country.flags.svg} alt={`${country.name.common} coat of arms`} style={{ height: "200px", padding: "0.25rem" }} /></TableCell>
+                        </>)
+                        :
+                        (<TableCell colSpan={2} align="center" >
+                          <img src={country.coatOfArms.svg} alt={`${country.name.common} coat of arms`} style={{ height: "200px", marginBottom: "1rem" }} />
+                          <img src={country.flags.svg} alt={`${country.name.common} coat of arms`} style={{ height: "200px" }} />
+                        </TableCell>)}
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Official name</TableCell>
+                      <TableCell >{country.name.official}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Common name</TableCell>
+                      <TableCell >{country.name.common}</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>Capital</TableCell>
+                      <TableCell>{country.capital}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Official languages</TableCell>
+                      <TableCell>{formatLanguages(country.languages)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Official currency</TableCell>
+                      <TableCell>{formatCurrencies(country.currencies)}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Population</TableCell>
+                      <TableCell>{country.population.toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Bordering countries</TableCell>
+                      <TableCell>{neighbouringCountries}</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Box>
             <Box>
-              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-              <Typography >{weather.weather[0].description}</Typography>
-              <Typography>Humidity {weather.main.humidity}%</Typography>
             </Box>
-
           </Box>
         </Box>
       )}
       <Box>
-        <Typography>Current Weather</Typography>
         <TableContainer component={Paper} sx={{ maxWidth: "550px" }}>
           <Table>
             <TableHead>
               <TableRow>
+                <TableCell>{country.capital} current weather</TableCell>
                 <TableCell><ThermostatIcon />Temperature</TableCell>
                 <TableCell><AirIcon />Wind</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               <TableRow>
+                <TableCell rowSpan={2} align="center"><img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].main} /></TableCell>
                 <TableCell>Current {weather.main.temp}°C</TableCell>
                 <TableCell>{weather.wind.speed} m/s</TableCell>
               </TableRow>
@@ -105,6 +175,7 @@ const CountriesSingle = () => {
                 <TableCell>Highest {weather.wind.gust} m/s</TableCell>
               </TableRow>
               <TableRow>
+                <TableCell>{weather.weather[0].main}</TableCell>
                 <TableCell>Lowest {weather.main.temp_min}°C</TableCell>
                 <TableCell>Feels like {weather.main.feels_like}°C</TableCell>
               </TableRow>
@@ -122,7 +193,7 @@ const CountriesSingle = () => {
             sm: "500px"
           },
           width: {
-            xs:"300px",
+            xs: "300px",
             sm: "500px"
           }
         }} >
