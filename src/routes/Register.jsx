525fd234ec20a0dafc6b27fa37e2eb5ca18447e3
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-17 22:24:12 +0200

    Update styles and remove Bootstrap

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth, registerWithEmailAndPassword } from "../auth/firebase"
 import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 const Register = () => {
     const [email, setEmail] = useState("");
@@ -11,7 +11,8 @@ const Register = () => {
     const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
 
-    const register = () => {
+    const register = (event) => {
+        event.preventDefault();
         if (!name) alert("Please enter your name");
         registerWithEmailAndPassword(name, email, password);
     }
@@ -23,30 +24,39 @@ const Register = () => {
     }, [loading, user])
 
     return (
-        <div>
-            <h1>Register</h1>
-            <input
-                type="text"
-                value={name}
-                placeholder="Full name"
-                onChange={(event) => setName(event.target.value)}
-            />
-            <input
-                type="text"
-                value={email}
-                placeholder="Email"
-                onChange={(event) => setEmail(event.target.value)}
-            />
-            <input
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button onClick={register} >Submit</Button>
-        </div>
+        <Box sx={{ width: "100vw", height: { xs: "calc(100vh - 56px)", sm: "calc(100vh - 64px)" } }}>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "2rem", justifyContent: "center", alignItems: "center", height: "100%" }}
+                component={"form"} onSubmit={register}>
+                <Typography variant="h1" sx={{ fontSize: "2rem" }} component={"h1"}>Register</Typography>
+                <TextField
+                    label="Full name"
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    required
+                    color="secondary"
+                />
+                <TextField
+                    label="Email"
+                    type="text"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    required
+                    color="secondary"
+                />
+                <TextField
+                    type="password"
+                    value={password}
+                    label="Password"
+                    onChange={(event) => setPassword(event.target.value)}
+                    required
+                    color="secondary"
+                />
+                <Button type="submit" color="secondary" variant="contained">Submit</Button>
+                <Typography mx={"3rem"} sx={{textAlign:"center"}}>Your email is stored for authentication purposes only</Typography>
+            </Box>
+        </Box>
     )
-
 }
 
 export default Register
