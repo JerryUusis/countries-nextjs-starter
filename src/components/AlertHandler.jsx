36b4c1676fa6642f8dc65e4b71d8b2716309ed83
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-20 17:01:50 +0200

    Initialize AlertHandler component

---
+++ src/components/AlertHandler.jsx
@@ -0,0 +1,39 @@
+import { Alert, AlertTitle, Fade } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { turnInvisible } from "../store/favouritesSlice";
+
+const AlertHandler = () => {
+    const faveAddedMessage = useSelector((state) => state.favourites.alertMessage);
+    const faveSeverity = useSelector((state) => state.favourites.alertSeverity);
+    const faveVisible = useSelector((state) => state.favourites.alertVisible);
+
+    const dispatch = useDispatch();
+
+    const hideAlertAfterDelay = () => {
+        setTimeout(() => {
+            dispatch(turnInvisible(false)); // Dispatch action to turn alert invisible
+        }, 2000); // 2000 milliseconds = 2 seconds
+    };
+
+    // Trigger the hideAlertAfterDelay function when faveVisible changes
+    useEffect(() => {
+        if (faveVisible) {
+            hideAlertAfterDelay();
+        }
+    }, [faveVisible]);
+
+    return (
+        <Fade sx={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "5" }} in={faveVisible} timeout={250} onExited={() => dispatch(turnInvisible(false))}>
+            <Alert
+                severity={faveSeverity}
+                variant="filled">
+                <AlertTitle textTransform="capitalize">
+                    {faveSeverity ? faveSeverity : null}
+                </AlertTitle>
+                {faveAddedMessage}</Alert>
+        </Fade>
+    )
+}
+
+export default AlertHandler
