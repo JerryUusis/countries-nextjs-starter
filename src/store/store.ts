0a5d3f82e48c1bc455ab3405fe08cd44f824be90
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-20 11:34:42 +0300

    Add AppDispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "./countriesSlice";
 import visitedCountriesReducer from "./visitedCountriesSlice";
 
-export default configureStore({
+export const store =  configureStore({
   reducer: {
     countries: countriesReducer,
     visitedCountries: visitedCountriesReducer,
   },
 });
+
+// Represents the dispatch function of the Redux store
+export type AppDispatch = typeof store.dispatch;
