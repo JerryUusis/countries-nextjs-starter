c508568828d7e0436ce51e9d806625dbb40a636e
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-21 12:45:34 +0300

    Separate data tables into components and import them

--- src/routes/CountriesSingle.tsx
+++ src/routes/CountriesSingle.tsx
@@ -1,32 +1,16 @@
-import {
-  Box,
-  Button,
-  CircularProgress,
-  TableContainer,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Paper,
-  useMediaQuery,
-  Typography,
-} from "@mui/material";
+import { Box, Button, CircularProgress } from "@mui/material";
 import { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { VITE_GOOGLE_API, VITE_GOOGLE_MAP_ID } from "../utils/config";
-import AirIcon from "@mui/icons-material/Air";
-import ThermostatIcon from "@mui/icons-material/Thermostat";
 import { APIProvider, Map } from "@vis.gl/react-google-maps"; //https://visgl.github.io/react-google-maps/
-import { formatCurrencies, formatLanguages } from "../utils/helperFunctions";
 import { initializeCountries } from "../store/countriesSlice";
-import { useSelector, useDispatch } from "react-redux";
-import NavigationIcon from "@mui/icons-material/Navigation";
+import { useDispatch } from "react-redux";
 import { AppDispatch } from "../store/store";
-import { CountriesStateType } from "../types/reduxStateTypes";
 import { WeatherData } from "../types/weather";
 import { getWeather } from "../services/weatherService";
 import { Country } from "../types/country";
+import WeatherTable from "../components/WeatherTable";
+import CountryDataTable from "../components/CountryDataTable";
 
 const CountriesSingle = () => {
   const [weather, setWeather] = useState<WeatherData>();
@@ -37,41 +21,14 @@ const CountriesSingle = () => {
   const navigate = useNavigate();
   const country: Country = location.state.country;
   const dispatch: AppDispatch = useDispatch();
-  const countriesList = useSelector(
-    (state: CountriesStateType) => state.countries.countries
-  );
 
-  const screenSizeSm = useMediaQuery("(min-width:600px)");
+  const position = { lat: weather?.coord.lat, lng: weather?.coord.lon };
+  // Center Maps to capital
+  const defaultCenter = { lat: position.lat, lng: position.lng };
 
   useEffect(() => {
     dispatch(initializeCountries());
   }, []);
-
-  const getNeighbouringCountries = (): React.ReactNode[] => {
-    const neighbouringCountries: React.ReactNode[] = [];
-    if (!country.borders || country.borders.length === 0) {
-      return neighbouringCountries;
-    }
-
-    for (const countryCioc of country.borders) {
-      countriesList.forEach((neighbour) => {
-        if (countryCioc === neighbour.cioc) {
-          neighbouringCountries.push(
-            <Link
-              to={`/countries/${neighbour.name.common}`}
-              state={{ country: neighbour }}
-              key={neighbour.name.common}
-            >
-              {neighbour.name.common} {neighbour.flag}
-            </Link>
-          );
-        }
-      });
-    }
-    return neighbouringCountries;
-  };
-
-  const neighbouringCountries = getNeighbouringCountries();
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -96,11 +53,6 @@ const CountriesSingle = () => {
     );
   }
 
-  const position = { lat: weather?.coord.lat, lng: weather?.coord.lon };
-  // Center Maps to capital
-  const defaultCenter = { lat: position.lat, lng: position.lng };
-  const windAngle = weather?.wind.deg;
-
   return (
     <Box
       sx={{
@@ -112,159 +64,9 @@ const CountriesSingle = () => {
       }}
     >
       {!error && weather && (
-        <Box>
-          <TableContainer
-            component={Paper}
-            sx={{ my: { xs: "0", sm: "2rem" } }}
-          >
-            <Table>
-              <TableBody>
-                <TableRow sx={{ backgroundColor: "lightGrey" }}>
-                  <TableCell colSpan={2} align="center">
-                    <Typography component={"h1"} sx={{ fontSize: "2rem" }}>
-                      {country.name.common}
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-                <TableRow sx={{ backgroundColor: "lightGrey" }}>
-                  {screenSizeSm ? (
-                    <>
-                      <TableCell>
-                        <img
-                          src={country.coatOfArms.svg}
-                          alt={`${country.name.common} coat of arms`}
-                          style={{ height: "200px" }}
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <img
-                          src={country.flags.svg}
-                          alt={`${country.name.common} coat of arms`}
-                          style={{ height: "200px" }}
-                        />
-                      </TableCell>
-                    </>
-                  ) : (
-                    <TableCell colSpan={2} align="center">
-                      <img
-                        src={country.coatOfArms.svg}
-                        alt={`${country.name.common} coat of arms`}
-                        style={{ width: "325px", marginBottom: "1rem" }}
-                      />
-                      <img
-                        src={country.flags.svg}
-                        alt={`${country.name.common} coat of arms`}
-                        style={{ width: "325px" }}
-                      />
-                    </TableCell>
-                  )}
-                </TableRow>
-                <TableRow>
-                  <TableCell>Official name</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {country.name.official}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Common name</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {country.name.common}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Capital</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {country.capital} ({position.lat?.toFixed(2)}°N,{" "}
-                    {position.lng?.toFixed(2)}°E)
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Official languages</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {formatLanguages(country.languages)}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Official currency</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {formatCurrencies(country.currencies)}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Population</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>
-                    {country.population.toLocaleString()}
-                  </TableCell>
-                </TableRow>
-                {neighbouringCountries.length > 0 ? (
-                  <TableRow>
-                    <TableCell>Bordering countries</TableCell>
-                    <TableCell sx={{ fontWeight: "bold" }}>
-                      {getNeighbouringCountries()}
-                    </TableCell>
-                  </TableRow>
-                ) : null}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
+        <CountryDataTable country={country} weather={weather} />
       )}
-      <Box>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>{country.capital} current weather</TableCell>
-                <TableCell>
-                  <Box sx={{ display: "flex", gap:"0.5rem" }}>
-                    <ThermostatIcon />
-                    Temperature
-                  </Box>
-                </TableCell>
-                <TableCell>
-                  <Box sx={{ display: "flex", gap:"0.5rem" }}>
-                    <AirIcon />
-                    Wind
-                  </Box>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell rowSpan={2} align="center">
-                  <img
-                    src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
-                    alt={weather?.weather[0].main}
-                  />
-                </TableCell>
-                <TableCell>Current {weather?.main.temp.toFixed(0)}°C</TableCell>
-                <TableCell>
-                  {weather?.wind.speed.toFixed(0)} m/s{" "}
-                  <NavigationIcon
-                    sx={{
-                      ml: "0.125rem",
-                      transform: `rotate(${windAngle}deg)`,
-                      fontSize: "1rem",
-                      opacity: "0.75",
-                    }}
-                  />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Max {weather?.main.temp_max.toFixed(0)}°C</TableCell>
-                <TableCell>
-                  Feels like {weather?.main.feels_like.toFixed(0)}°C
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>{weather?.weather[0].main}</TableCell>
-                <TableCell>Min {weather?.main.temp_min.toFixed(0)}°C</TableCell>
-                <TableCell>Humidity {weather?.main.humidity} %</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
+      {weather ? <WeatherTable country={country} weather={weather} /> : null}
       <Button
         variant="contained"
         color="secondary"
