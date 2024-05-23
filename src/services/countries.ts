a18cfd766bfb2f578ece5af68fc3ecbfc99f26b8
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-23 15:51:29 +0300

    Add logic to add countries to indexedDB

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,10 +1,30 @@
 import axios from "axios";
+import { initializeDB } from "../utils/indexedDb";
 
-const countriesAPI = "https://restcountries.com/v3.1/all"
+const countriesAPI = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
+// If indexedDB is empty, fetch countries directly from API and populate indexedDB
 const getAll = async () => {
-    const response = await axios.get(countriesAPI)
-    return response.data
-}
+  try {
+    const db = await initializeDB();
+    const transaction = db.transaction("countries", "readonly");
+    const store = transaction.objectStore("countries");
+    const countriesArray = await store.get("all");
 
-export default { getAll }
+    if (!countriesArray) {
+      const response = await axios.get(countriesAPI);
+      if (response.data) {
+        const transaction = db.transaction("countries", "readwrite");
+        const store = transaction.objectStore("countries");
+        await store.put(response.data, "all");
+        return response.data;
+      }
+    }
+    return countriesArray;
+  } catch (error) {
+    console.error("Error fetching countries data:", error);
+    throw error;
+  }
+};
+
+export default { getAll };
