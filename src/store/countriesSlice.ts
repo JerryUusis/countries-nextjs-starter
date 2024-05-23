794e9bb898f4a41118fb5697afd43e264b0bade7
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-23 15:48:59 +0300

    Update initializeCountries to have a finally block

--- src/store/countriesSlice.ts
+++ src/store/countriesSlice.ts
@@ -5,11 +5,15 @@ import { Country } from "../types/country";
 
 export const initializeCountries = () => {
   return async (dispatch: Dispatch) => {
-    const countries: Country[] = await countriesService.getAll();
-    dispatch(getCountries(countries));
-    setTimeout(() => {
+    try {
+      const countries: Country[] = await countriesService.getAll();
+      dispatch(getCountries(countries));
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
       dispatch(isLoading(false));
-    }, 1000);
+    }
   };
 };
 
@@ -22,7 +26,6 @@ export const countriesSlice = createSlice({
   reducers: {
     getCountries(state, action) {
       state.countries = action.payload;
-      state.isLoading = false;
     },
     isLoading: (state, action) => {
       state.isLoading = action.payload;
